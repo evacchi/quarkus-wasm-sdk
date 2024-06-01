eeb692272d1457a9fbdc54783c4ec6d0d98a21b3
Edoardo Vacchi <evacchi@users.noreply.github.com>
2024-06-01 10:38:38 +0200

    plugin
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -2,4 +2,5 @@ build:
 	tinygo build -target wasi -o plugin.wasm main.go
 
 test:
-	extism call plugin.wasm greet --input "world" --wasi
+	extism call plugin.wasm request_headers \
+		--input '{"headers":{"Content-Type":["application/json"]}}' --wasi
--- main.go
+++ main.go
@@ -4,12 +4,34 @@ import (
 	"github.com/extism/go-pdk"
 )
 
-//go:export greet
-func Greet() int32 {
-	name := pdk.InputString()
-	pdk.OutputString("Hello, " + name)
+type Request struct {
+	Headers map[string][]string `json:"headers"`
+}
+
+//go:export request_headers
+func RequestHeaders() int32 {
+	req := &Request{}
+	err := pdk.InputJSON(req)
+	if err != nil {
+		pdk.SetError(err)
+		return 1
+	}
+
+	req.appendHeaders("X-Wasm-Plugin", "Hello World!")
+
+	err = pdk.OutputJSON(req)
+	if err != nil {
+		pdk.SetError(err)
+		return 1
+	}
 	return 0
 }
 
-func main() {}
+func (r *Request) appendHeaders(k, v string) {
+	hs := r.Headers[k]
+	hs = append(hs, v)
+	r.Headers[k] = hs
 
+}
+
+func main() {}
